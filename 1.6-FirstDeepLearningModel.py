2173f67787bf2b2b072aa68602afd7fdb2ca9ee2
coreyauger <coreyauger@gmail.com>
2018-03-22 14:55:56 -0700

    model 1

---
+++ 1.6-FirstDeepLearningModel.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets.samples_generator import make_circles
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+
+
+X, y = make_circles(n_samples=1000,
+                    noise=0.1,
+                    factor=0.2,
+                    random_state=0)
+
+print(X.shape)           
+plt.figure(figsize=(5, 5))
+plt.plot(X[y==0, 0], X[y==0, 1], 'ob', alpha=0.5)
+plt.plot(X[y==1, 0], X[y==1, 1], 'xr', alpha=0.5)
+plt.xlim(-1.5, 1.5)
+plt.ylim(-1.5, 1.5)
+plt.legend(['0', '1'])
+plt.title("Blue circles and Red crosses")
+plt.show()
+
+model = Sequential()
+model.add(Dense(4, input_shape=(2,), activation='tanh'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(SGD(lr=0.5), 'binary_crossentropy', metrics=['accuracy'])
+model.fit(X, y, epochs=20)
+
+# generate 101 points from -1.5 to +1.5
+hticks = np.linspace(-1.5, 1.5, 101)
+vticks = np.linspace(-1.5, 1.5, 101)
+# create a grid of points ab
+aa, bb = np.meshgrid(hticks, vticks)
+ab = np.c_[aa.ravel(), bb.ravel()]
+
+c = model.predict(ab)
+cc = c.reshape(aa.shape)
+
+plt.figure(figsize=(5, 5))
+plt.contourf(aa, bb, cc, cmap='bwr', alpha=0.2)     # this is where we overly the boundry 
+plt.plot(X[y==0, 0], X[y==0, 1], 'ob', alpha=0.5)
+plt.plot(X[y==1, 0], X[y==1, 1], 'xr', alpha=0.5)
+plt.xlim(-1.5, 1.5)
+plt.ylim(-1.5, 1.5)
+plt.legend(['0', '1'])
+plt.title("Blue circles and Red crosses")
+plt.show()
